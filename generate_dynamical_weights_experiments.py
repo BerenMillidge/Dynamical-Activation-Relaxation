e51729541efd36c4afac08a44c4304430c4ffb41
Beren <bmillidgework@gmail.com>
2020-09-15 14:32:39 +0100

    fix names

--- generate_dynamical_weights_experiments.py
+++ generate_dynamical_weights_experiments.py
@@ -4,7 +4,7 @@ generated_name = str(sys.argv[1])
 log_path = str(sys.argv[2])
 save_path = str(sys.argv[3])
 exp_name = str(sys.argv[4])
-bcall = "python main.py"
+base_call = "python main.py"
 output_file = open(generated_name, "w")
 seeds = 1
 datasets = ["mnist","fashion"]
@@ -12,7 +12,7 @@ datasets = ["mnist","fashion"]
 lrs = [0.8,0.5,0.1]
 num_inference_steps = [500,300,200100,50]
 for dataset in datasets:
-    base_call = bcall + " --dataset " + str(dataset)
+    base_call = base_cell + " --dataset " + str(dataset)
     condition=dataset+"_inference_lrs"
     for lr in lrs:
         bcall = base_call +" --inference_learning_rate " + str(lr)
@@ -31,7 +31,7 @@ for dataset in datasets:
         for s in range(seeds):
             lpath = log_path + "/"+str(exp_name) +"_"+cond + "/" + str(s)
             spath = save_path + "/" + str(exp_name) +"_"+cond + "/" + str(s)
-            final_call = base_call + " --logdir " + str(lpath) + " --savedir " + str(spath) + " --dynamical_weight_update True --inference_lr 0.5"
+            final_call = bcall + " --logdir " + str(lpath) + " --savedir " + str(spath) + " --dynamical_weight_update True --inference_lr 0.5"
             print(final_call)
             print(final_call, file=output_file)
 
