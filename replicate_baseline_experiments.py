e9edd76c22dfbef115dcb0c9a302606531357a1c
Beren <bmillidgework@gmail.com>
2020-09-16 13:14:53 +0100

    Add files via upload

---
+++ replicate_baseline_experiments.py
@@ -0,0 +1,17 @@
+import os
+import sys
+generated_name = str(sys.argv[1])
+log_path = str(sys.argv[2])
+save_path = str(sys.argv[3])
+exp_name = str(sys.argv[4])
+base_call = "python main.py"
+output_file = open(generated_name, "w")
+seeds = 1
+datasets = ["mnist","fashion"]
+for dataset in datasets:
+    base_call = bcall + " --dataset " + str(dataset)
+    for s in range(seeds):
+        spath = save_path + "/" + str(exp_name) +"_"+condition + "/" + str(s)
+        final_call = base_call + " --logdir " + str(lpath) + " --savedir " + str(spath)
+        print(final_call)
+        print(final_call, file=output_file)
