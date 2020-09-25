d07d8894e799c2a87a5d5305c2da75c12da815dd
Beren <bmillidgework@gmail.com>
2020-09-25 19:28:52 +0100

    experiment switches with act fns

--- generate_cnn_experiments.py
+++ generate_cnn_experiments.py
@@ -8,70 +8,74 @@ bcall = "python cnn.py"
 output_file = open(generated_name, "w")
 seeds = 1
 datasets = ["cifar","cifar100","svhn"]
+act_fns = ["tanh","relu"]
 
 for dataset in datasets:
     base_call = bcall + " --dataset " + str(dataset)
+    cond = dataset + "_"
 
-    condition=dataset+"_default"
-    for s in range(seeds):
-        lpath = log_path + "/"+str(exp_name) +"_"+condition + "/" + str(s)
-        spath = save_path + "/" + str(exp_name) +"_"+condition + "/" + str(s)
-        final_call = base_call + " --logdir " + str(lpath) + " --savedir " + str(spath)
-        print(final_call)
-        print(final_call, file=output_file)
+    for act_fn in act_fns:
+        base_call += " --act_fn " + str(act_fn)
+        condition= cond + act_fn+"_default"
+        for s in range(seeds):
+            lpath = log_path + "/"+str(exp_name) +"_"+condition + "/" + str(s)
+            spath = save_path + "/" + str(exp_name) +"_"+condition + "/" + str(s)
+            final_call = base_call + " --logdir " + str(lpath) + " --savedir " + str(spath)
+            print(final_call)
+            print(final_call, file=output_file)
 
-    condition=dataset+"_use_everything"
-    for s in range(seeds):
-        lpath = log_path + "/"+str(exp_name) +"_"+condition + "/" + str(s)
-        spath = save_path + "/" + str(exp_name) +"_"+condition + "/" + str(s)
-        final_call = base_call + " --logdir " + str(lpath) + " --savedir " + str(spath) + " --use_conv_backwards_nonlinearity False --use_FC_backwards_nonlinearity False --use_FC_backwards_weights True"
-        print(final_call)
-        print(final_call, file=output_file)
+        condition= cond + act_fn+"_use_everything"
+        for s in range(seeds):
+            lpath = log_path + "/"+str(exp_name) +"_"+condition + "/" + str(s)
+            spath = save_path + "/" + str(exp_name) +"_"+condition + "/" + str(s)
+            final_call = base_call + " --logdir " + str(lpath) + " --savedir " + str(spath) + " --use_conv_backwards_nonlinearity False --use_FC_backwards_nonlinearity False --use_FC_backwards_weights True"
+            print(final_call)
+            print(final_call, file=output_file)
+            
+        condition= cond + act_fn+"_use_FC_backwards_weights"
+        for s in range(seeds):
+            lpath = log_path + "/"+str(exp_name) +"_"+condition + "/" + str(s)
+            spath = save_path + "/" + str(exp_name) +"_"+condition + "/" + str(s)
+            final_call = base_call + " --logdir " + str(lpath) + " --savedir " + str(spath) + " --use_FC_backwards_weights True"
+            print(final_call)
+            print(final_call, file=output_file)
+
+        condition= cond + act_fn+"_use_FC_backwards_nonlinearity"
+        for s in range(seeds):
+            lpath = log_path + "/"+str(exp_name) +"_"+condition + "/" + str(s)
+            spath = save_path + "/" + str(exp_name) +"_"+condition + "/" + str(s)
+            final_call = base_call + " --logdir " + str(lpath) + " --savedir " + str(spath) + " --use_FC_backwards_nonlinearity False"
+            print(final_call)
+            print(final_call, file=output_file)
+            
+        condition= cond + act_fn+"_use_conv_backwards_weights"
+        for s in range(seeds):
+            lpath = log_path + "/"+str(exp_name) +"_"+condition + "/" + str(s)
+            spath = save_path + "/" + str(exp_name) +"_"+condition + "/" + str(s)
+            final_call = base_call + " --logdir " + str(lpath) + " --savedir " + str(spath) + " --use_conv_backwards_weights True"
+            print(final_call)
+            print(final_call, file=output_file)
         
-    condition=dataset+"_use_FC_backwards_weights"
-    for s in range(seeds):
-        lpath = log_path + "/"+str(exp_name) +"_"+condition + "/" + str(s)
-        spath = save_path + "/" + str(exp_name) +"_"+condition + "/" + str(s)
-        final_call = base_call + " --logdir " + str(lpath) + " --savedir " + str(spath) + " --use_FC_backwards_weights True"
-        print(final_call)
-        print(final_call, file=output_file)
+        condition= cond + act_fn+"_use_conv_backwards_nonlinearity"
+        for s in range(seeds):
+            lpath = log_path + "/"+str(exp_name) +"_"+condition + "/" + str(s)
+            spath = save_path + "/" + str(exp_name) +"_"+condition + "/" + str(s)
+            final_call = base_call + " --logdir " + str(lpath) + " --savedir " + str(spath) + " --use_conv_backwards_nonlinearity False"
+            print(final_call)
+            print(final_call, file=output_file)
+            
+        condition= cond + act_fn+"_use_FC_both"
+        for s in range(seeds):
+            lpath = log_path + "/"+str(exp_name) +"_"+condition + "/" + str(s)
+            spath = save_path + "/" + str(exp_name) +"_"+condition + "/" + str(s)
+            final_call = base_call + " --logdir " + str(lpath) + " --savedir " + str(spath) + " --use_FC_backwards_weights True --use_FC_backwards_nonlinearity False"
+            print(final_call)
+            print(final_call, file=output_file)
 
-    condition=dataset+"_use_FC_backwards_nonlinearity"
-    for s in range(seeds):
-        lpath = log_path + "/"+str(exp_name) +"_"+condition + "/" + str(s)
-        spath = save_path + "/" + str(exp_name) +"_"+condition + "/" + str(s)
-        final_call = base_call + " --logdir " + str(lpath) + " --savedir " + str(spath) + " --use_FC_backwards_nonlinearity False"
-        print(final_call)
-        print(final_call, file=output_file)
-        
-    condition=dataset+"_use_conv_backwards_weights"
-    for s in range(seeds):
-        lpath = log_path + "/"+str(exp_name) +"_"+condition + "/" + str(s)
-        spath = save_path + "/" + str(exp_name) +"_"+condition + "/" + str(s)
-        final_call = base_call + " --logdir " + str(lpath) + " --savedir " + str(spath) + " --use_conv_backwards_weights True"
-        print(final_call)
-        print(final_call, file=output_file)
-    
-    condition=dataset+"_use_conv_backwards_nonlinearity"
-    for s in range(seeds):
-        lpath = log_path + "/"+str(exp_name) +"_"+condition + "/" + str(s)
-        spath = save_path + "/" + str(exp_name) +"_"+condition + "/" + str(s)
-        final_call = base_call + " --logdir " + str(lpath) + " --savedir " + str(spath) + " --use_conv_backwards_nonlinearity False"
-        print(final_call)
-        print(final_call, file=output_file)
-        
-    condition=dataset+"_use_FC_both"
-    for s in range(seeds):
-        lpath = log_path + "/"+str(exp_name) +"_"+condition + "/" + str(s)
-        spath = save_path + "/" + str(exp_name) +"_"+condition + "/" + str(s)
-        final_call = base_call + " --logdir " + str(lpath) + " --savedir " + str(spath) + " --use_FC_backwards_weights True --use_FC_backwards_nonlinearity False"
-        print(final_call)
-        print(final_call, file=output_file)
-
-    condition=dataset+"_use_all_nonlinearity"
-    for s in range(seeds):
-        lpath = log_path + "/"+str(exp_name) +"_"+condition + "/" + str(s)
-        spath = save_path + "/" + str(exp_name) +"_"+condition + "/" + str(s)
-        final_call = base_call + " --logdir " + str(lpath) + " --savedir " + str(spath) + " --use_conv_backwards_nonlinearity False --use_FC_backwards_nonlinearity False"
-        print(final_call)
-        print(final_call, file=output_file)
+        condition= cond + act_fn+"_use_all_nonlinearity"
+        for s in range(seeds):
+            lpath = log_path + "/"+str(exp_name) +"_"+condition + "/" + str(s)
+            spath = save_path + "/" + str(exp_name) +"_"+condition + "/" + str(s)
+            final_call = base_call + " --logdir " + str(lpath) + " --savedir " + str(spath) + " --use_conv_backwards_nonlinearity False --use_FC_backwards_nonlinearity False"
+            print(final_call)
+            print(final_call, file=output_file)
--- generate_full_experiment_set.py
+++ generate_full_experiment_set.py
@@ -8,61 +8,64 @@ bcall = "python main.py"
 output_file = open(generated_name, "w")
 seeds = 1
 datasets = ["mnist","fashion"]
+act_fns = ["tanh","relu"]
 
 for dataset in datasets:
-    base_call = bcall + " --dataset " + str(dataset)
-    condition=dataset+"_use_current_x_weights"
-    for s in range(seeds):
-        lpath = log_path + "/"+str(exp_name) +"_"+condition + "/" + str(s)
-        spath = save_path + "/" + str(exp_name) +"_"+condition + "/" + str(s)
-        final_call = base_call + " --logdir " + str(lpath) + " --savedir " + str(spath) + " --use_current_x_weights True"
-        print(final_call)
-        print(final_call, file=output_file)
+    cond = dataset + "_"
+    for act_fn in act_fns:
+        base_call = bcall + " --dataset " + str(dataset) + " --act_fn " + str(act_fn)
+        condition= cond + act_fn+"_use_current_x_weights"
+        for s in range(seeds):
+            lpath = log_path + "/"+str(exp_name) +"_"+condition + "/" + str(s)
+            spath = save_path + "/" + str(exp_name) +"_"+condition + "/" + str(s)
+            final_call = base_call + " --logdir " + str(lpath) + " --savedir " + str(spath) + " --use_current_x_weights True"
+            print(final_call)
+            print(final_call, file=output_file)
+            
+        condition= cond + act_fn+"_use_fderiv_x_update"
+        for s in range(seeds):
+            lpath = log_path + "/"+str(exp_name) +"_"+condition + "/" + str(s)
+            spath = save_path + "/" + str(exp_name) +"_"+condition + "/" + str(s)
+            final_call = base_call + " --logdir " + str(lpath) + " --savedir " + str(spath) + " --use_fderiv_x_update True "
+            print(final_call)
+            print(final_call, file=output_file)
+
+        condition= cond + act_fn+"_use_fderiv_weight_update"
+        for s in range(seeds):
+            lpath = log_path + "/"+str(exp_name) +"_"+condition + "/" + str(s)
+            spath = save_path + "/" + str(exp_name) +"_"+condition + "/" + str(s)
+            final_call = base_call + " --logdir " + str(lpath) + " --savedir " + str(spath) + " --use_fderiv_weight_update True"
+            print(final_call)
+            print(final_call, file=output_file)
+
+        condition= cond + act_fn+"_use_fderiv_both"
+        for s in range(seeds):
+            lpath = log_path + "/"+str(exp_name) +"_"+condition + "/" + str(s)
+            spath = save_path + "/" + str(exp_name) +"_"+condition + "/" + str(s)
+            final_call = base_call + " --logdir " + str(lpath) + " --savedir " + str(spath) + " --use_fderiv_x_update True --use_fderiv_weight_update True"
+            print(final_call)
+            print(final_call, file=output_file)
         
-    condition=dataset+"_use_fderiv_x_update"
-    for s in range(seeds):
-        lpath = log_path + "/"+str(exp_name) +"_"+condition + "/" + str(s)
-        spath = save_path + "/" + str(exp_name) +"_"+condition + "/" + str(s)
-        final_call = base_call + " --logdir " + str(lpath) + " --savedir " + str(spath) + " --use_fderiv_x_update True "
-        print(final_call)
-        print(final_call, file=output_file)
+        condition= cond + act_fn+"_use_xnext_fderiv_x"
+        for s in range(seeds):
+            lpath = log_path + "/"+str(exp_name) +"_"+condition + "/" + str(s)
+            spath = save_path + "/" + str(exp_name) +"_"+condition + "/" + str(s)
+            final_call = base_call + " --logdir " + str(lpath) + " --savedir " + str(spath) + " --use_fderiv_x_update True  --xnext_fderiv_x True"
+            print(final_call)
+            print(final_call, file=output_file)
 
-    condition=dataset+"_use_fderiv_weight_update"
-    for s in range(seeds):
-        lpath = log_path + "/"+str(exp_name) +"_"+condition + "/" + str(s)
-        spath = save_path + "/" + str(exp_name) +"_"+condition + "/" + str(s)
-        final_call = base_call + " --logdir " + str(lpath) + " --savedir " + str(spath) + " --use_fderiv_weight_update True"
-        print(final_call)
-        print(final_call, file=output_file)
+        condition= cond + act_fn+"_use_xnext_fderiv_weights"
+        for s in range(seeds):
+            lpath = log_path + "/"+str(exp_name) +"_"+condition + "/" + str(s)
+            spath = save_path + "/" + str(exp_name) +"_"+condition + "/" + str(s)
+            final_call = base_call + " --logdir " + str(lpath) + " --savedir " + str(spath) + " --use_fderiv_weight_update True --xnext_fderiv_weights True"
+            print(final_call)
+            print(final_call, file=output_file)
 
-    condition=dataset+"_use_fderiv_both"
-    for s in range(seeds):
-        lpath = log_path + "/"+str(exp_name) +"_"+condition + "/" + str(s)
-        spath = save_path + "/" + str(exp_name) +"_"+condition + "/" + str(s)
-        final_call = base_call + " --logdir " + str(lpath) + " --savedir " + str(spath) + " --use_fderiv_x_update True --use_fderiv_weight_update True"
-        print(final_call)
-        print(final_call, file=output_file)
-    
-    condition=dataset+"_use_xnext_fderiv_x"
-    for s in range(seeds):
-        lpath = log_path + "/"+str(exp_name) +"_"+condition + "/" + str(s)
-        spath = save_path + "/" + str(exp_name) +"_"+condition + "/" + str(s)
-        final_call = base_call + " --logdir " + str(lpath) + " --savedir " + str(spath) + " --use_fderiv_x_update True  --xnext_fderiv_x True"
-        print(final_call)
-        print(final_call, file=output_file)
-
-    condition=dataset+"_use_xnext_fderiv_weights"
-    for s in range(seeds):
-        lpath = log_path + "/"+str(exp_name) +"_"+condition + "/" + str(s)
-        spath = save_path + "/" + str(exp_name) +"_"+condition + "/" + str(s)
-        final_call = base_call + " --logdir " + str(lpath) + " --savedir " + str(spath) + " --use_fderiv_weight_update True --xnext_fderiv_weights True"
-        print(final_call)
-        print(final_call, file=output_file)
-
-    condition=dataset+"_use_xnext_fderiv_both"
-    for s in range(seeds):
-        lpath = log_path + "/"+str(exp_name) +"_"+condition + "/" + str(s)
-        spath = save_path + "/" + str(exp_name) +"_"+condition + "/" + str(s)
-        final_call = base_call + " --logdir " + str(lpath) + " --savedir " + str(spath) + " --use_fderiv_x_update True --use_fderiv_weight_update True --xnext_fderiv_x True --xnext_fderiv_weights True"
-        print(final_call)
-        print(final_call, file=output_file)
+        condition= cond + act_fn+"_use_xnext_fderiv_both"
+        for s in range(seeds):
+            lpath = log_path + "/"+str(exp_name) +"_"+condition + "/" + str(s)
+            spath = save_path + "/" + str(exp_name) +"_"+condition + "/" + str(s)
+            final_call = base_call + " --logdir " + str(lpath) + " --savedir " + str(spath) + " --use_fderiv_x_update True --use_fderiv_weight_update True --xnext_fderiv_x True --xnext_fderiv_weights True"
+            print(final_call)
+            print(final_call, file=output_file)
