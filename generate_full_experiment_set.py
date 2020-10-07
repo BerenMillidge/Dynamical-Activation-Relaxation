8bacc6a81433e5d5b2e8457383bb91914f0f1179
Beren <bmillidgework@gmail.com>
2020-10-07 14:13:17 +0100

    Add files via upload

--- generate_full_experiment_set.py
+++ generate_full_experiment_set.py
@@ -15,7 +15,7 @@ for dataset in datasets:
     for act_fn in act_fns:
         base_call = bcall + " --dataset " + str(dataset) + " --act_fn " + str(act_fn)
         condition= cond + act_fn+"_use_current_x_weights"
-        for s in range(1,seeds+1):
+        for s in range(7,12):
             lpath = log_path + "/"+str(exp_name) +"_"+condition + "/" + str(s)
             spath = save_path + "/" + str(exp_name) +"_"+condition + "/" + str(s)
             final_call = base_call + " --logdir " + str(lpath) + " --savedir " + str(spath) + " --use_current_x_weights True"
@@ -23,7 +23,7 @@ for dataset in datasets:
             print(final_call, file=output_file)
             
         condition= cond + act_fn+"_use_fderiv_x_update"
-        for s in range(1,seeds+1):
+        for s in range(7,12):
             lpath = log_path + "/"+str(exp_name) +"_"+condition + "/" + str(s)
             spath = save_path + "/" + str(exp_name) +"_"+condition + "/" + str(s)
             final_call = base_call + " --logdir " + str(lpath) + " --savedir " + str(spath) + " --use_fderiv_x_update True "
@@ -31,7 +31,7 @@ for dataset in datasets:
             print(final_call, file=output_file)
 
         condition= cond + act_fn+"_use_fderiv_weight_update"
-        for s in range(1,seeds+1):
+        for s in range(7,12):
             lpath = log_path + "/"+str(exp_name) +"_"+condition + "/" + str(s)
             spath = save_path + "/" + str(exp_name) +"_"+condition + "/" + str(s)
             final_call = base_call + " --logdir " + str(lpath) + " --savedir " + str(spath) + " --use_fderiv_weight_update True"
@@ -39,7 +39,7 @@ for dataset in datasets:
             print(final_call, file=output_file)
 
         condition= cond + act_fn+"_use_fderiv_both"
-        for s in range(1,seeds+1):
+        for s in range(7,12):
             lpath = log_path + "/"+str(exp_name) +"_"+condition + "/" + str(s)
             spath = save_path + "/" + str(exp_name) +"_"+condition + "/" + str(s)
             final_call = base_call + " --logdir " + str(lpath) + " --savedir " + str(spath) + " --use_fderiv_x_update True --use_fderiv_weight_update True"
